0fd33a20570bdfed0ed2ea15cfee4403e2c9e524
Bruno Novais <novais99bruno@gmail.com>
2024-05-09 21:19:03 -0300

    storing information in cache

--- src/app/clima/page.js
+++ src/app/clima/page.js
@@ -8,7 +8,7 @@ import ClimaCard from '@/components/ClimaCard';
 import ClimaSemanal from '@/components/ClimaSemanal';
 
 export default function Clima() {
-    const [cidade, setCidade] = useState('');
+    const [cidade, setCidade] = useState(localStorage.getItem('ultimaCidadePesquisada') || '');
     const [data, setData] = useState();
 
     useEffect(() => {
@@ -20,6 +20,8 @@ export default function Clima() {
                         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&lang=pt_br&units=metric&appid=42e29d143e6e7b8595bc2a484ea78108`);
                         setData(response.data);
                         setCidade(response.data.name);
+                        localStorage.setItem('ultimaCidadePesquisada', response.data.name); // Armazena a cidade no localStorage
+                        localStorage.setItem('clima', JSON.stringify(response.data)); // Armazena a previsão do clima no localStorage
                     });
                 } else {
                     console.error('Geolocalização não suportada neste navegador.');
@@ -30,23 +32,29 @@ export default function Clima() {
         };
 
         buscarClima();
-    }, []);
 
-    const iconUrl = data ? `http://openweathermap.org/img/wn/${data.weather[0].icon}.png` : '';
+        if (cidade) {
+            buscarClimaPorNome();
+        }
+    }, [cidade]);
+
+    const iconUrl = data? `http://openweathermap.org/img/wn/${data.weather[0].icon}.png` : '';
 
     const buscarClimaPorNome = async () => {
         try {
             const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&lang=pt_br&units=metric&appid=42e29d143e6e7b8595bc2a484ea78108`);
             setData(response.data);
+            localStorage.setItem('ultimaCidadePesquisada', cidade);
+            localStorage.setItem('clima', JSON.stringify(response.data)); // Atualiza a previsão do clima no localStorage
         } catch (error) {
             console.error(error);
         }
     };
+
     return (
         <main className="flex flex-col w-full min-h-screen p-8 bg-gray-900 items-center center space-y-6">
-
             <div className='flex flex-row items-center justify-between w-full space-x-2'>
-                <Link href="/page">
+                <Link href="/">
                     <Image
                         src="/weather.png"
                         alt="Imagem de Fundo"
@@ -56,30 +64,16 @@ export default function Clima() {
                 </Link>
             </div>
 
-            {/* <div className='flex flex-row'>
-                <div className="relative w-full">
-                    <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite o nome da cidade" value={cidade}
-                        onChange={(e) => setCidade(e.target.value)}
-                        required />
-                </div>
-                <button type="submit" onClick={buscarClima} className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                    </svg>
-                    <span className="sr-only">Search</span>
-                </button>
-            </div> */}
-
             <section class="bg-white dark:bg-gray-900">
                 <div class="py-8 px-4 mx-auto max-w-screen-xl text-center">
                     <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-5xl dark:text-white">Explore a previsão do tempo local</h1>
                     <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Permita que o navegador acesse sua localização ou pesquise a cidade abaixo.</p>
 
                     <div className='flex flex-col w-full items-center justify-center'>
-                        {!data ? (
+                        {!data? (
                             <div class="flex items-center p-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 mb-6" role="alert">
                                 <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                                    <path d="M10.5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10.5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
                                 </svg>
                                 <span class="sr-only">Info</span>
                                 <div>
@@ -106,8 +100,6 @@ export default function Clima() {
                 </div>
             </section>
 
-
-
             {data && (
                 <div className='flex flex-row w-full space-x-6'>
                     <div className='flex flex-col p-6 border border-gray-200 rounded-lg shadow dark:bg-slate-900 dark:border-gray-700 space-y-6'>
@@ -117,8 +109,6 @@ export default function Clima() {
                             name={data.name}
                             temperature={data.main.temp}
                             description={data.weather[0].description}
-                            minTemperature={data.main.temp_min}
-                            maxTemperature={data.main.temp_max}
                         />
                     </div>
 
@@ -128,17 +118,12 @@ export default function Clima() {
                 </div>
             )}
 
-
             {data && (
                 <div className='flex flex-col space-y-6 p-6 w-full border border-gray-200 rounded-lg shadow dark:bg-slate-900 dark:border-gray-700'>
-
                     <div><span class="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-1.5 rounded dark:bg-green-900 dark:text-green-300">Previsão dos próximos 5 dias</span></div>
                     <ClimaSemanal cidade={cidade} />
                 </div>
-
             )}
-
-
         </main>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -45,12 +45,12 @@ export default function Home() {
         width={180}
         height={180}
       />
-      <section class="bg-white dark:bg-gray-900">
-        <div class="flex flex-col space-y-8 py-8 px-4 mx-auto max-w-screen-xl text-center">
-          <h1 class="text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-5xl dark:text-white">Vença o clima!</h1>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="flex flex-col space-y-8 py-8 px-4 mx-auto max-w-screen-xl text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-5xl dark:text-white">Vença o clima!</h1>
 
 
-          <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Desafie a Natureza! Descubra a previsão do tempo da sua cidade e esteja preparado para qualquer clima.</p>
+          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Desafie a Natureza! Descubra a previsão do tempo da sua cidade e esteja preparado para qualquer clima.</p>
 
 
           <div className='flex flex-row w-full justify-center'>
@@ -68,13 +68,13 @@ export default function Home() {
                       maxTemperature={data.main.temp_max}
                     />
                   ) : (
-                    <div class="flex items-center p-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
-                      <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <div className="flex items-center p-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+                      <svg className="flexShrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
                       </svg>
-                      <span class="sr-only">Info</span>
+                      <span className="sr-only">Info</span>
                       <div>
-                        <span class="font-medium">Permita que o navegador acesse sua localização!</span>
+                        <span className="font-medium">Permita que o navegador acesse sua localização!</span>
                       </div>
                     </div>
                   )}
@@ -82,43 +82,19 @@ export default function Home() {
 
               </div>
               {data && (
-                <div class="flex flex-col justify-between p-6 leading-normal items-center">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Previsão atual em {data.name}</h5>
-                  <p class="font-normal text-gray-700 dark:text-gray-400 w-80">Não seja pego desprevenido, confira a previsão completa.</p>
+                <div className="flex flex-col justify-between p-6 leading-normal items-center">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Previsão atual em {data.name}</h5>
+                  <p className="font-normal text-gray-700 dark:text-gray-400 w-80">Não seja pego desprevenido, confira a previsão completa.</p>
                 </div>
               )}
             </div>
           </div>
 
-          {/* <div className="flex w-full items-center justify-center">
-            {data && data.name ? (
-              <ClimaCard
-                iconUrl={iconUrl}
-                name={data.name}
-                temperature={data.main.temp}
-                description={data.weather[0].description}
-                minTemperature={data.main.temp_min}
-                maxTemperature={data.main.temp_max}
-              />
-            ) : (
-              <div class="flex items-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
-                <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-                </svg>
-                <span class="sr-only">Info</span>
-                <div>
-                  <span class="font-medium">Permita que o navegador acesse sua localização!</span>
-                </div>
-              </div>
-            )}
-          </div> */}
-
-
-          <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-            <a href="/clima" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a href="/clima" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
               Conferir Previsão Completa
-              <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+              <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
               </svg>
             </a>
           </div>
